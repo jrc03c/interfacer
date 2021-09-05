5cd36121aa798c83f3e87de135f1c9965e61d2d4
jrc03c <jrc03c@pm.me>
2021-09-04 20:47:42 -0500

    ...

--- interfacer.js
+++ interfacer.js
@@ -2,20 +2,33 @@ class Interfacer {
   constructor() {
     const self = this
 
+    function helper(ifaces) {
+      ifaces.forEach(iface => {
+        Object.keys(iface).forEach(key => {
+          if (!self[key]) {
+            self[key] = () => {
+              throw new Error(`This object must implement a "${key}" method!`)
+            }
+          }
+        })
+      })
+    }
+
     Object.defineProperty(self, "interfaces", {
       configurable: false,
       enumerable: false,
 
       set(ifaces) {
-        ifaces.forEach(iface => {
-          Object.keys(iface).forEach(key => {
-            if (!self[key]) {
-              self[key] = () => {
-                throw new Error(`This object must implement a "${key}" method!`)
-              }
-            }
-          })
-        })
+        helper(ifaces)
+      },
+    })
+
+    Object.defineProperty(self, "implements", {
+      configurable: false,
+      enumerable: false,
+
+      set(ifaces) {
+        helper(ifaces)
       },
     })
   }
